01f6640f74a3c11f3110244f69693f08114bf9a6
Aniket Jena <75910246+killerdroid99@users.noreply.github.com>
2022-03-07 02:11:02 +0530

    Configured placeMarker function to input desired marker

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const GameBoard = (() => {
 	const connectToDOM = (array) => {
 		array.forEach((element, i) => {
 			document.getElementById(`x${i}`).innerText = element;
-			// element.addEventListener("click", () => {
-			// 	element.innerText = "X";
-			// });
 		});
 	};
 
@@ -20,20 +17,92 @@ const GameBoard = (() => {
 const Player = (marker) => {
 	"use strict";
 	const myMarker = marker;
+	return { myMarker };
+};
+
+const GameFlow = (() => {
+	"use strict";
+	const playerOne = Player("");
+	const playerTwo = Player("");
 	const board = GameBoard;
-	const placeMarker = (marker, board) => {
+
+	const handleChoice = () => {
+		const xMarker = document.getElementById("x");
+		const oMarker = document.getElementById("o");
+		xMarker.addEventListener("click", () => {
+			playerOne.myMarker = xMarker.value;
+			playerTwo.myMarker = oMarker.value;
+			document.getElementById("overlay").style.display = "none";
+			document.querySelector(
+				".p1text"
+			).innerText = `Player 1 plays ${playerOne.myMarker}`;
+		});
+		oMarker.addEventListener("click", () => {
+			playerOne.myMarker = oMarker.value;
+			playerTwo.myMarker = xMarker.value;
+			document.getElementById("overlay").style.display = "none";
+			document.querySelector(
+				".p1text"
+			).innerText = `Player 1 plays ${playerOne.myMarker}`;
+		});
+	};
+
+	const placeMarker = (board) => {
+		let turn = true;
 		board.forEach((element, i) => {
 			element = document.getElementById(`x${i}`);
 			element.addEventListener("click", () => {
 				if (element.innerText === "") {
-					element.innerText = marker;
+					if (turn) {
+						element.innerText = playerOne.myMarker;
+						document.querySelector(".p1text").innerText = "";
+						document.querySelector(
+							".p2text"
+						).innerText = `Player 2 plays ${playerTwo.myMarker}`;
+						board[i] = playerOne.myMarker;
+						rules();
+						turn = false;
+					} else {
+						element.innerText = playerTwo.myMarker;
+						document.querySelector(
+							".p1text"
+						).innerText = `Player 1 plays ${playerOne.myMarker}`;
+						document.querySelector(".p2text").innerText = "";
+						board[i] = playerTwo.myMarker;
+						rules();
+						turn = true;
+					}
 				}
+				console.log(board);
 			});
 		});
 	};
-	placeMarker(myMarker, board);
-	return { myMarker };
-};
 
-const player_one = Player("✖️");
-const player_two = Player("⭕");
+	const rules = () => {
+		if (
+			board[0] === playerOne.myMarker ||
+			(board[0] === playerTwo.myMarker && board[1] === playerOne.myMarker) ||
+			(board[1] === playerTwo.myMarker && board[2] === playerOne.myMarker) ||
+			board[2] === playerTwo.myMarker
+		) {
+			console.log("test win");
+		} else if (
+			board[3] === playerOne.myMarker ||
+			(board[3] === playerTwo.myMarker && board[4] === playerOne.myMarker) ||
+			(board[4] === playerTwo.myMarker && board[5] === playerOne.myMarker) ||
+			board[5] === playerTwo.myMarker
+		) {
+			console.log("test win");
+		} else if (
+			board[6] === playerOne.myMarker ||
+			(board[6] === playerTwo.myMarker && board[7] === playerOne.myMarker) ||
+			(board[7] === playerTwo.myMarker && board[8] === playerOne.myMarker) ||
+			board[8] === playerTwo.myMarker
+		) {
+			console.log("test win");
+		}
+	};
+
+	handleChoice();
+	placeMarker(board);
+})();
